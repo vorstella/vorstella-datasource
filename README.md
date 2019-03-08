8e1867ee57fca31df8539943bafe5af24a4c551e
Matthew Johnston <warmwaffles@gmail.com>
2019-03-08 09:19:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vorstella Datasource
+
+Add vorstella as a datasource for your grafana dashboard.
---
+++ src/datasource.js
@@ -0,0 +1,141 @@
+import _ from "lodash";
+
+export class GenericDatasource {
+
+  constructor(instanceSettings, $q, backendSrv, templateSrv) {
+    this.type = instanceSettings.type;
+    this.url = "https://metrics.dev.noc.vorstella.com";
+    this.name = instanceSettings.name;
+    this.q = $q;
+    this.backendSrv = backendSrv;
+    this.templateSrv = templateSrv;
+    this.headers = {
+      'Content-Type': 'application/json'
+    };
+
+    console.log(instanceSettings);
+  }
+
+  query(options) {
+    var query = this.buildQueryParameters(options);
+    query.targets = query.targets.filter(t => !t.hide);
+
+    if (query.targets.length <= 0) {
+      return this.q.when({data: []});
+    }
+
+    if (this.templateSrv.getAdhocFilters) {
+      query.adhocFilters = this.templateSrv.getAdhocFilters(this.name);
+    } else {
+      query.adhocFilters = [];
+    }
+
+    return this.backendSrv.datasourceRequest({
+      url: '/metrics/api/v1/clusters',
+      method: 'GET'
+    });
+  }
+
+  testDatasource() {
+    return {
+      status: "success",
+      message: "Data source is working.",
+      title: "Success"
+    };
+  }
+
+  annotationQuery(options) {
+    var query = this.templateSrv.replace(options.annotation.query, {}, 'glob');
+    var annotationQuery = {
+      range: options.range,
+      annotation: {
+        name: options.annotation.name,
+        datasource: options.annotation.datasource,
+        enable: options.annotation.enable,
+        iconColor: options.annotation.iconColor,
+        query: query
+      },
+      rangeRaw: options.rangeRaw
+    };
+
+    return this.doRequest({
+      url: this.url + '/annotations',
+      method: 'get',
+      data: annotationQuery
+    }).then(result => {
+      return result.data;
+    });
+  }
+
+  metricFindQuery(query) {
+    var interpolated = {
+        target: this.templateSrv.replace(query, null, 'regex')
+    };
+
+    return this.doRequest({
+      url: this.url + '/search',
+      data: interpolated,
+      method: 'POST',
+    }).then(this.mapToTextValue);
+  }
+
+  mapToTextValue(result) {
+    return _.map(result.data, (d, i) => {
+      if (d && d.text && d.value) {
+        return { text: d.text, value: d.value };
+      } else if (_.isObject(d)) {
+        return { text: d, value: i};
+      }
+      return { text: d, value: d };
+    });
+  }
+
+  doRequest(options) {
+    return ;
+  }
+
+  buildQueryParameters(options) {
+    //remove placeholder targets
+    options.targets = _.filter(options.targets, target => {
+      return target.target !== 'select metric';
+    });
+
+    var targets = _.map(options.targets, target => {
+      return {
+        target: this.templateSrv.replace(target.target, options.scopedVars, 'regex'),
+        refId: target.refId,
+        hide: target.hide,
+        type: target.type || 'timeserie'
+      };
+    });
+
+    options.targets = targets;
+
+    return options;
+  }
+
+  getTagKeys(options) {
+    return new Promise((resolve, reject) => {
+      this.doRequest({
+        url: this.url + '/tag-keys',
+        method: 'POST',
+        data: options
+      }).then(result => {
+        return resolve(result.data);
+      });
+    });
+  }
+
+  getTagValues(options) {
+    return new Promise((resolve, reject) => {
+      this.doRequest({
+        url: this.url + '/tag-values',
+        method: 'POST',
+        data: options
+      }).then(result => {
+        return resolve(result.data);
+      });
+    });
+  }
+
+}
